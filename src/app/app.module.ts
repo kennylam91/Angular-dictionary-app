c51eff9c3192762b081ff62635ebbe1e08e10a34
kennylam91 <phamvanlam1991@gmail.com>
2019-08-12 09:55:18 +0700

    add component word-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DictionaryHomepageComponent } from './dictionary-homepage/dictionary-homepage.component';
 import { DictionaryWordDetailComponent } from './dictionary-word-detail/dictionary-word-detail.component';
+import { DictionaryWordsListComponent } from './dictionary-words-list/dictionary-words-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DictionaryHomepageComponent,
-    DictionaryWordDetailComponent
+    DictionaryWordDetailComponent,
+    DictionaryWordsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dictionary-words-list/dictionary-words-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary-words-list',
+  templateUrl: './dictionary-words-list.component.html',
+  styleUrls: ['./dictionary-words-list.component.css']
+})
+export class DictionaryWordsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
