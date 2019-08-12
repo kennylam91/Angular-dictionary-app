144a049f186029ed16d7bb3c9aa51a422d80512a
kennylam91 <phamvanlam1991@gmail.com>
2019-08-12 10:01:29 +0700

    add word.service with wordList

---
+++ src/app/word.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {IWord} from './dictionary-words-list/word';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WordService {
+  // tslint:disable-next-line:variable-name
+  private _wordList: IWord = [
+    {english: 'hello', vietnamese: 'xin chao'},
+    {english: 'good morning', vietnamese: 'chao buoi sang'},
+  ];
+
+  get wordList(): IWord {
+    return this._wordList;
+  }
+
+  set wordList(value: IWord) {
+    this._wordList = value;
+  }
+
+  constructor() {
+  }
+}
