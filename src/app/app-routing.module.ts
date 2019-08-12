ba7c506335c10feefeb58525c30e32ded3e3a0fe
kennylam91 <phamvanlam1991@gmail.com>
2019-08-12 10:19:03 +0700

    add children route of dictionary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {DictionaryHomepageComponent} from './dictionary-homepage/dictionary-homepage.component';
+import {DictionaryWordDetailComponent} from './dictionary-word-detail/dictionary-word-detail.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'dictionary', component: DictionaryHomepageComponent},
+  {
+    path: 'dictionary', component: DictionaryHomepageComponent,
+    children: [
+      {path: ':word', component: DictionaryWordDetailComponent}]
+  },
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
